cfde8bbd82ba54c6a5fb0a93bef6c553e329fe0b
danish40416 <152517567+danish40416@users.noreply.github.com>
2024-01-28 18:16:58 +0500

    Update code

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import {Image} from 'react-bootstrap';
 
 export default function Navbar() {
   return (
     <div>
         <div className="navbar">
         <div className="d-flex justify-content-start">
-         React Todos
+          <Image src={require('../images/logo.png')} className="logo ms-3 mt-2" alt="Logo" />
         </div>
       </div>
     </div>
